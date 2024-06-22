96e3e6c662678c7a179166f29c29952f5deb9aaa
SHMOIL OWAIS K <164631851+urstrulyshmoil@users.noreply.github.com>
2024-06-22 13:31:42 +0530

    Add files via upload
    DOM & INTERFACE

---
+++ script.js
@@ -0,0 +1,21 @@
+const divs=document.querySelectorAll('div');
+const button=document.querySelector('button');
+
+
+function logText(e){
+    console.log(this.classList.value);
+    // e.stopPropogation();
+    // console.log(this);
+
+}
+
+
+
+divs.forEach(div => div.addEventListener('click',logText,{capture:false,once:true}));
+
+
+button.addEventListener('click',()=>{
+    console.log('Click!!!');   
+},{
+    once:true
+})
